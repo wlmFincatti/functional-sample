383e9a619f9dcc2a9d7d6b6047e7de21214b85b7
William da Silva <williamfincatti.dasilva@gmail.com>
2023-10-03 16:04:31 -0300

    refactor: change methods to receive user as parameter

--- src/main/scala/examplesfp/ExampleComposeMonad.scala
+++ src/main/scala/examplesfp/ExampleComposeMonad.scala
@@ -8,40 +8,49 @@ object ExampleComposeMonad extends App {
 
   private case class Coordinates(latLong: Option[(Double, Double)])
 
-  private val user =
+  private val userWithCoordinates =
     User("William", 35, Some(Address("Rua 1", 10, Some("Casa 1"), Some(Coordinates(Some(-23.5505, -46.6333))))))
 
-  private def imperativeMethod(): Unit = {
+  private val userWithoutCoordinates =
+    User("William", 35, Some(Address("Rua 1", 10, Some("Casa 1"), None)))
+
+  private def imperativeMethod(user: User): Unit = {
     if (user.address.isDefined) {
       if (user.address.get.coordinates.isDefined) {
         if (user.address.get.coordinates.get.latLong.isDefined) {
           println(
             s"User live in coordinates latitude: ${user.address.get.coordinates.get.latLong.get._1}, longitude: ${user.address.get.coordinates.get.latLong.get._2}"
           )
+        } else {
+          println("The user does not have valid coordinates")
         }
+      } else {
+        println("The user does not have valid coordinates")
       }
     }
   }
 
-  private def functionalMethod(): Unit = {
+  private def functionalMethod(user: User): Unit = {
     user.address
       .flatMap(_.coordinates)
       .flatMap(_.latLong)
       .map(latLong => s"User live in coordinates latitude: ${latLong._1}, longitude: ${latLong._2}")
-      .fold(println("User don't have coordinates"))(println)
+      .fold(println("The user does not have valid coordinates"))(println)
   }
 
-  private def functionalWithForComprehensionMethod(): Unit = {
-    for {
+  private def functionalWithForComprehensionMethod(user: User): Unit = {
+    (for {
       address     <- user.address
       coordinates <- address.coordinates
       latLong     <- coordinates.latLong
     } yield {
       println(s"User live in coordinates latitude: ${latLong._1}, longitude: ${latLong._2}")
+    }) getOrElse {
+      println("The user does not have valid coordinates")
     }
   }
 
-  imperativeMethod()
-  functionalMethod()
-  functionalWithForComprehensionMethod()
+  imperativeMethod(userWithCoordinates)
+  functionalMethod(userWithCoordinates)
+  functionalWithForComprehensionMethod(userWithCoordinates)
 }
